0975869ec478addf50b3e7b66869c61bc7d427ad
ElizabetaDuleva <bettyhikova@abv.bg>
2023-04-09 13:15:41 +0300

    Homework 3

--- src/main/java/homework3/DaysOfTheWeek.java
+++ src/main/java/homework3/DaysOfTheWeek.java
@@ -6,36 +6,37 @@ public class DaysOfTheWeek {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter а number from 1 to 7");
-        int a = scanner.nextInt();
+        int weekDayNumber = scanner.nextInt();
+        String dayOfTheWeek = null;
 
-            switch (a) {
+            switch (weekDayNumber) {
                 case 1:
-                    System.out.printf("Monday\n");
+                    dayOfTheWeek = "Monday\n";
                     break;
                 case 2:
-                    System.out.printf("Tuesday\n");
+                    dayOfTheWeek = "Tuesday\n";
                     break;
                 case 3:
-                    System.out.printf("Wednesday\n");
+                    dayOfTheWeek = ("Wednesday\n");
                     break;
                 case 4:
-                    System.out.printf("Thursday\n");
+                    dayOfTheWeek = ("Thursday\n");
                     break;
                 case 5:
-                    System.out.printf("Friday\n");
+                    dayOfTheWeek = ("Friday\n");
                     break;
                 case 6:
-                    System.out.printf("Saturday\n");
+                    dayOfTheWeek = ("Saturday\n");
                     break;
                 case 7:
-                    System.out.printf("Sunday\n");
+                    dayOfTheWeek = ("Sunday\n");
                     break;
                 default:
-                    System.out.print("Incorrect information\n");
+                    dayOfTheWeek = ("Incorrect information\n");
 
             }
-            String DayOfTheWeek;
-            System.out.println("You have selected " + a + " day of the week is " ); // I don't know how to аdd the day at the end
+
+            System.out.println("You have selected " + weekDayNumber + " day of the week is " + dayOfTheWeek) ;
     }
 
 }
